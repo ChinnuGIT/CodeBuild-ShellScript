f96719abe063305b0f4a994032546c1ff70ce9a5
ChinnuGIT <69529240+ChinnuGIT@users.noreply.github.com>
2023-06-06 19:18:40 +0530

    Update Stop-task.sh

--- Stop-task.sh
+++ Stop-task.sh
@@ -2,13 +2,11 @@
 set -eou pipefail
 
 CLUSTERNAME="tomcat-cluster"
+REGION="us-east-2"
 if [ -z "$CLUSTERNAME" ]; then
     echo "You must specify a cluster to stop all tasks on"
     exit 1
 fi
-
-CLUSTERNAME=$(jq -r .clusterName <"./clusters/$CLUSTERNAME")
-REGION=$(jq -r .region <"./clusters/$CLUSTERNAME")
 
 for task in $(aws ecs list-tasks --region "$REGION" --cluster "$CLUSTERNAME" | jq -r ".taskArns[]"); do
     aws ecs stop-task --region "$REGION" --task "$task" --cluster "$CLUSTERNAME" &
