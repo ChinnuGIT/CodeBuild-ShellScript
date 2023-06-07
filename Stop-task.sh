b2f82f1e80508660a964dd7e5662c2c173c91a6a
Chinnu <chinmay.kp.111@gamil.com>
2023-06-07 13:51:45 +0530

    modified

--- Stop-task.sh
+++ Stop-task.sh
@@ -11,6 +11,7 @@ fi
 for task in $(aws ecs list-tasks --region "$REGION" --cluster "$CLUSTERNAME" | jq -r ".taskArns[]"); do
     aws ecs stop-task --region "$REGION" --task "$task" --cluster "$CLUSTERNAME" &
     sleep 0.25
+    aws ecs delete-task-definitions --region "$REGION" --task "$task" --cluster "$CLUSTERNAME"
 done
 
 wait
